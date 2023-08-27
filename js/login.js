ec92f483946611c71d09c080503401f65b28c4f8
diyorbek1234567 <yusupovamatluba>
2023-08-27 21:29:38 +0500

    loading has changed

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
-const loading = document.getElementById("loading");
+const loading = document.querySelector(".loader");
 
-const loadingDuration = 2000; // 2s
+const loadingDuration = 3000; // 2s
 
 setTimeout(() => {
   loading.classList.add('loading-none');
